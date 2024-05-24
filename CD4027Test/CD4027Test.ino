94cbfafa11ff897016786fa2a61c590ba9687921
Thomas Quinot <thomas@quinot.org>
2024-05-25 00:17:22 +0200

    Add CD4027 test

---
+++ CD4027Test/CD4027Test.ino
@@ -0,0 +1,151 @@
+// Test behaviour of a CD4027B JK flip-flop
+
+#include <LibPrintf.h>
+
+// Arduino pins        FF1 pins  FF2 pins
+
+// Inputs
+
+#define PIN_SET   3 // FF1:  9   FF2: 7
+#define PIN_J     4 // FF1: 10   FF2: 6
+#define PIN_K     5 // FF1: 11   FF2: 5
+#define PIN_RESET 6 // FF1: 12   FF2: 4
+#define PIN_CLOCK 7 // FF1: 13   FF2: 3
+
+// Outputs
+
+#define PIN_Qi    8 // FF1: 14   FF2: 2
+#define PIN_Q     9 // FF1: 15   FF2: 1
+
+bool checkOutputs(int q, int qi, char *msg) {
+  int actual_q = digitalRead(PIN_Q);
+  int actual_qi = digitalRead(PIN_Qi);
+  
+  if (q == actual_q && qi == actual_qi)
+    return true;
+
+  printf("!! %s: expected Q=%d Qi=%d, got Q=%d Qi=%d !! ", msg, q, qi, actual_q, actual_qi);
+  return false;
+}
+
+bool checkState(int q, char *msg) {
+  return checkOutputs(q, !q, msg);
+}
+
+void pulse(int pin) {
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(pin, LOW);
+  delayMicroseconds(1);
+}
+
+void pulseClock() {
+  int q, qi;
+
+  digitalWrite(PIN_CLOCK, HIGH);
+  delayMicroseconds(1);
+  q = digitalRead(PIN_Q);
+  qi = digitalRead(PIN_Qi);
+  digitalWrite(PIN_CLOCK, LOW);
+  delayMicroseconds(1);
+
+  checkOutputs(q, qi, "Unexpected change on clock falling edge");
+}
+
+void setState(int q) {
+  if (q)
+    pulse(PIN_SET);
+  else
+    pulse(PIN_RESET);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(PIN_SET,   OUTPUT);
+  pinMode(PIN_J,     OUTPUT);
+  pinMode(PIN_K,     OUTPUT);
+  pinMode(PIN_RESET, OUTPUT);
+  pinMode(PIN_CLOCK, OUTPUT);
+
+  pinMode(PIN_Qi,    INPUT);
+  pinMode(PIN_Q,     INPUT);
+}
+
+#define CSI "\x1b["
+
+bool reportResult(bool pass) {
+  if (pass)
+    printf(CSI "32m" "PASS" CSI "0m" "\n");
+  else
+    printf(CSI "30;41m" "FAIL" CSI "0m" "\n");
+  return pass;
+}
+bool testTransition(int j, int k, int q, int expected_q) {
+  printf("J=%d K=%d Q=%d ", j, k, q, expected_q);
+  setState(q);
+  if (!checkState(q, "Failed to check pre-state"))
+    return false;
+
+  digitalWrite(PIN_J, j);
+  digitalWrite(PIN_K, k);
+  pulseClock();
+  
+  return reportResult(checkState(expected_q, "Unexpected check after clock"));
+}
+
+void runTests() {
+  printf("\n\n**** Starting test series ****\n\n");
+
+  // Rule 1: J=1 K=X Q=0 => Q=1
+  testTransition(1, 0, 0, 1);
+  testTransition(1, 1, 0, 1);
+
+  // Rule 2: J=X K=0 Q=1 => Q=1
+  testTransition(0, 0, 1, 1);
+  testTransition(1, 0, 1, 1);
+
+  // Rule 3: J=0 K=X Q=0 => Q=0
+  testTransition(0, 0, 0, 0);
+  testTransition(0, 1, 0, 0);
+
+  // Rule 4: J=X K=1 Q=1 => Q=0
+  testTransition(0, 1, 1, 0);
+  testTransition(1, 1, 1, 0);
+
+  // Rule 5: no output change on clock falling edge
+
+  // Rule 6: SET
+
+  printf("SET ");
+  setState(1);
+  reportResult(checkState(1, "Failed to SET state"));
+
+  // Rule 7: RESET
+
+  printf("RESET ");
+  setState(0);
+  reportResult(checkState(0, "Failed to RESET state"));
+
+  // Rule 8: SET+RESET
+
+  printf("SET+RESET ");
+  digitalWrite(PIN_SET, HIGH);
+  digitalWrite(PIN_RESET, HIGH);
+  delayMicroseconds(1);
+  reportResult(checkOutputs(1, 1, "Unexpected state on SET+RESET"));
+
+  // Cleanup
+
+  digitalWrite(PIN_SET, LOW);
+  digitalWrite(PIN_RESET, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printf("\n\nPress return to start test...");
+  while (!(Serial.available() && Serial.read() == '\n')) ;
+
+  runTests();
+}
---
+++ CD4027Test/README.md
@@ -0,0 +1,4 @@
+CD4027Test
+==========
+
+Test whether a CD4027 IC behaves according to the [TI data sheet](https://www.ti.com/lit/ds/symlink/cd4027b.pdf).
